e6e520bc4489474a6d6b67318d9329afa95bae50
Collin <collindching@gmail.com>
2019-07-27 23:27:14 -0700

    add exercises

---
+++ ex40.py
@@ -0,0 +1,11 @@
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])
+
+"""
+A module is a Python file with functions or variables in it
+
+You then import that file
+
+You can access the functions or variables in that module with the '.' (dot) operator
+"""
+
